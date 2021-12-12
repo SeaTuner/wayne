32bf3162292283c34fa317823c7ba950676c007a
SeaTuner <97007035+SeaTuner@users.noreply.github.com>
2021-12-12 06:16:55 +0000

    Better interface

---
+++ Code/Feat/genFeat_cooccurrence_tfidf_feat.py
@@ -0,0 +1,59 @@
+
+"""
+__file__
+
+    genFeat_cooccurrence_tfidf.py
+
+__description__
+
+    This file generates the following features for each run and fold, and for the entire training and testing set.
+
+        1. tfidf for the following cooccurrence terms
+            - query unigram/bigram & title unigram/bigram
+            - query unigram/bigram & description unigram/bigram
+            - query id & title unigram/bigram
+            - query id & description unigram/bigram
+
+        2. corresponding lsa (svd) version features
+
+__author__
+
+    Chenglong Chen < c.chenglong@gmail.com >
+
+"""
+
+import re
+import sys
+import cPickle
+import ngram
+from feat_utils import dump_feat_name
+from sklearn.decomposition import TruncatedSVD
+from nlp_utils import stopwords, english_stemmer, stem_tokens, getTFV
+sys.path.append("../")
+from param_config import config
+
+######################
+## Pre-process data ##
+######################
+token_pattern = r"(?u)\b\w\w+\b"
+#token_pattern = r'\w{1,}'
+#token_pattern = r"\w+"
+#token_pattern = r"[\w']+"
+def preprocess_data(line,
+                    token_pattern=token_pattern,
+                    exclude_stopword=config.cooccurrence_word_exclude_stopword,
+                    encode_digit=False):
+    token_pattern = re.compile(token_pattern, flags = re.UNICODE | re.LOCALE)
+    ## tokenize
+    tokens = [x.lower() for x in token_pattern.findall(line)]
+    ## stem
+    tokens_stemmed = stem_tokens(tokens, english_stemmer)
+    if exclude_stopword:
+        tokens_stemmed = [x for x in tokens_stemmed if x not in stopwords]
+    return tokens_stemmed
+
+
+########################
+## Cooccurrence terms ##
+########################
+def cooccurrence_terms(lst1, lst2, j
