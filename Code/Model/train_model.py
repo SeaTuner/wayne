59175c14327169d1f299e186c6ff8fbe4313ed62
SeaTuner <97007035+SeaTuner@users.noreply.github.com>
2021-12-12 19:36:08 +0000

    change travis config

--- Code/Model/train_model.py
+++ Code/Model/train_model.py
@@ -91,4 +91,111 @@ def hyperopt_wrapper(param, feat_folder, feat_name):
 
     print("        Model")
     print("              %s" % feat_name)
-    print("      
+    print("        Param")
+    for k,v in sorted(param.items()):
+        print("              %s: %s" % (k,v))
+    print("        Result")
+    print("                    Run      Fold      Bag      Kappa      Shape")
+
+    ## evaluate performance
+    kappa_cv_mean, kappa_cv_std = hyperopt_obj(param, feat_folder, feat_name, trial_counter)
+
+    ## log
+    var_to_log = [
+        "%d" % trial_counter,
+        "%.6f" % kappa_cv_mean, 
+        "%.6f" % kappa_cv_std
+    ]
+    for k,v in sorted(param.items()):
+        var_to_log.append("%s" % v)
+    writer.writerow(var_to_log)
+    log_handler.flush()
+
+    return {'loss': -kappa_cv_mean, 'attachments': {'std': kappa_cv_std}, 'status': STATUS_OK}
+    
+
+#### train CV and final model with a specified parameter setting
+def hyperopt_obj(param, feat_folder, feat_name, trial_counter):
+
+    kappa_cv = np.zeros((config.n_runs, config.n_folds), dtype=float)
+    for run in range(1,config.n_runs+1):
+        for fold in range(1,config.n_folds+1):
+            rng = np.random.RandomState(2015 + 1000 * run + 10 * fold)
+
+            #### all the path
+            path = "%s/Run%d/Fold%d" % (feat_folder, run, fold)
+            save_path = "%s/Run%d/Fold%d" % (output_path, run, fold)
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            # feat
+            feat_train_path = "%s/train.feat" % path
+            feat_valid_path = "%s/valid.feat" % path
+            # weight
+            weight_train_path = "%s/train.feat.weight" % path
+            weight_valid_path = "%s/valid.feat.weight" % path
+            # info
+            info_train_path = "%s/train.info" % path
+            info_valid_path = "%s/valid.info" % path
+            # cdf
+            cdf_valid_path = "%s/valid.cdf" % path
+            # raw prediction path (rank)
+            raw_pred_valid_path = "%s/valid.raw.pred.%s_[Id@%d].csv" % (save_path, feat_name, trial_counter)
+            rank_pred_valid_path = "%s/valid.pred.%s_[Id@%d].csv" % (save_path, feat_name, trial_counter)
+
+            ## load feat
+            X_train, labels_train = load_svmlight_file(feat_train_path)
+            X_valid, labels_valid = load_svmlight_file(feat_valid_path)
+            if X_valid.shape[1] < X_train.shape[1]:
+                X_valid = hstack([X_valid, np.zeros((X_valid.shape[0], X_train.shape[1]-X_valid.shape[1]))])
+            elif X_valid.shape[1] > X_train.shape[1]:
+                X_train = hstack([X_train, np.zeros((X_train.shape[0], X_valid.shape[1]-X_train.shape[1]))])
+            X_train = X_train.tocsr()
+            X_valid = X_valid.tocsr()
+            ## load weight
+            weight_train = np.loadtxt(weight_train_path, dtype=float)
+            weight_valid = np.loadtxt(weight_valid_path, dtype=float)
+
+            ## load valid info
+            info_train = pd.read_csv(info_train_path)
+            numTrain = info_train.shape[0]
+            info_valid = pd.read_csv(info_valid_path)
+            numValid = info_valid.shape[0]
+            Y_valid = info_valid["median_relevance"]
+            ## load cdf
+            cdf_valid = np.loadtxt(cdf_valid_path, dtype=float)
+
+            ## make evalerror func
+            evalerror_regrank_valid = lambda preds,dtrain: evalerror_regrank_cdf(preds, dtrain, cdf_valid)
+            evalerror_softmax_valid = lambda preds,dtrain: evalerror_softmax_cdf(preds, dtrain, cdf_valid)
+            evalerror_softkappa_valid = lambda preds,dtrain: evalerror_softkappa_cdf(preds, dtrain, cdf_valid)
+            evalerror_ebc_valid = lambda preds,dtrain: evalerror_ebc_cdf(preds, dtrain, cdf_valid, ebc_hard_threshold)
+            evalerror_cocr_valid = lambda preds,dtrain: evalerror_cocr_cdf(preds, dtrain, cdf_valid)
+
+            ##############
+            ## Training ##
+            ##############
+            ## you can use bagging to stabilize the predictions
+            preds_bagging = np.zeros((numValid, bagging_size), dtype=float)
+            for n in range(bagging_size):
+                if bootstrap_replacement:
+                    sampleSize = int(numTrain*bootstrap_ratio)
+                    index_base = rng.randint(numTrain, size=sampleSize)
+                    index_meta = [i for i in range(numTrain) if i not in index_base]
+                else:
+                    randnum = rng.uniform(size=numTrain)
+                    index_base = [i for i in range(numTrain) if randnum[i] < bootstrap_ratio]
+                    index_meta = [i for i in range(numTrain) if randnum[i] >= bootstrap_ratio]
+                
+                if param.has_key("booster"):
+                    dvalid_base = xgb.DMatrix(X_valid, label=labels_valid, weight=weight_valid)
+                    dtrain_base = xgb.DMatrix(X_train[index_base], label=labels_train[index_base], weight=weight_train[index_base])
+                        
+                    watchlist = []
+                    if verbose_level >= 2:
+                        watchlist  = [(dtrain_base, 'train'), (dvalid_base, 'valid')]
+                    
+                ## various models
+                if param["task"] in ["regression", "ranking"]:
+                    ## regression & pairwise ranking with xgboost
+                    bst = xgb.train(param, dtrain_base, param['num_round'], watchlist, feval=evalerror_regrank_valid)
+             
