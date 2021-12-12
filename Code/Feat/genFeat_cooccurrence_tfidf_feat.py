371bfafc8674b47e583d273608572c8526254fe1
SeaTuner <97007035+SeaTuner@users.noreply.github.com>
2021-12-12 12:24:22 +0000

    Update life.dm

--- Code/Feat/genFeat_cooccurrence_tfidf_feat.py
+++ Code/Feat/genFeat_cooccurrence_tfidf_feat.py
@@ -56,4 +56,116 @@ def preprocess_data(line,
 ########################
 ## Cooccurrence terms ##
 ########################
-def cooccurrence_terms(lst1, lst2, j
+def cooccurrence_terms(lst1, lst2, join_str):
+    terms = [""] * len(lst1) * len(lst2)
+    cnt =  0
+    for item1 in lst1:
+        for item2 in lst2:
+            terms[cnt] = item1 + join_str + item2
+            cnt += 1
+    res = " ".join(terms)
+    return res
+
+
+##################
+## Extract feat ##
+##################
+def extract_feat(df):
+    ## unigram
+    print "generate unigram"
+    df["query_unigram"] = list(df.apply(lambda x: preprocess_data(x["query"]), axis=1))
+    df["title_unigram"] = list(df.apply(lambda x: preprocess_data(x["product_title"]), axis=1))
+    df["description_unigram"] = list(df.apply(lambda x: preprocess_data(x["product_description"]), axis=1))
+    ## bigram
+    print "generate bigram"
+    join_str = "_"
+    df["query_bigram"] = list(df.apply(lambda x: ngram.getBigram(x["query_unigram"], join_str), axis=1))
+    df["title_bigram"] = list(df.apply(lambda x: ngram.getBigram(x["title_unigram"], join_str), axis=1))
+    df["description_bigram"] = list(df.apply(lambda x: ngram.getBigram(x["description_unigram"], join_str), axis=1))
+    # ## trigram
+    # join_str = "_"
+    # df["query_trigram"] = list(df.apply(lambda x: ngram.getTrigram(x["query_unigram"], join_str), axis=1))
+    # df["title_trigram"] = list(df.apply(lambda x: ngram.getTrigram(x["title_unigram"], join_str), axis=1))
+    # df["description_trigram"] = list(df.apply(lambda x: ngram.getTrigram(x["description_unigram"], join_str), axis=1))
+
+    ## cooccurrence terms
+    join_str = "X"
+    # query unigram
+    df["query_unigram_title_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["title_unigram"], join_str), axis=1))
+    df["query_unigram_title_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["title_bigram"], join_str), axis=1))
+    df["query_unigram_description_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["description_unigram"], join_str), axis=1))
+    df["query_unigram_description_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["description_bigram"], join_str), axis=1))
+    # query bigram
+    df["query_bigram_title_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["title_unigram"], join_str), axis=1))
+    df["query_bigram_title_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["title_bigram"], join_str), axis=1))
+    df["query_bigram_description_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["description_unigram"], join_str), axis=1))
+    df["query_bigram_description_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["description_bigram"], join_str), axis=1))
+    # query id
+    df["query_id_title_unigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["title_unigram"], join_str), axis=1))
+    df["query_id_title_bigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["title_bigram"], join_str), axis=1))
+    df["query_id_description_unigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["description_unigram"], join_str), axis=1))
+    df["query_id_description_bigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["description_bigram"], join_str), axis=1))
+
+        
+
+if __name__ == "__main__":
+
+    ############
+    ## Config ##
+    ############
+    ## cooccurrence terms column names
+    column_names = [
+        "query_unigram_title_unigram",
+        "query_unigram_title_bigram",
+        "query_unigram_description_unigram",
+        "query_unigram_description_bigram",
+        "query_bigram_title_unigram",
+        "query_bigram_title_bigram",
+        "query_bigram_description_unigram",
+        "query_bigram_description_bigram",
+        "query_id_title_unigram",
+        "query_id_title_bigram",
+        "query_id_description_unigram",
+        "query_id_description_bigram",
+    ]
+    ## feature names
+    feat_names = [ name+"_tfidf" for name in column_names ]
+    ## file to save feat names
+    feat_name_file = "%s/intersect_tfidf.feat_name" % config.feat_folder
+
+    ngram_range = config.cooccurrence_tfidf_ngram_range
+
+    svd_n_components = 100
+
+    ###############
+    ## Load Data ##
+    ###############
+    ## load data
+    with open(config.processed_train_data_path, "rb") as f:
+        dfTrain = cPickle.load(f)
+    with open(config.processed_test_data_path, "rb") as f:
+        dfTest = cPickle.load(f)
+    ## load pre-defined stratified k-fold index
+    with open("%s/stratifiedKFold.%s.pkl" % (config.data_folder, config.stratified_label), "rb") as f:
+            skf = cPickle.load(f)
+
+    #######################
+    ## Generate Features ##
+    #######################
+    print("==================================================")
+    print("Generate co-occurrence tfidf features...")
+
+    ## get cooccurrence terms
+    extract_feat(dfTrain)
+    extract_feat(dfTest)
+
+    ######################
+    ## Cross validation ##
+    ######################
+    print("For cross-validation...")
+    for run in range(config.n_runs):
+        ## use 33% for training and 67 % for validation
+        ## so we switch trainInd and validInd
+        for fold, (validInd, trainInd) in enumerate(skf[run]):
+            print("Run: %d, Fold: %d" % (run+1, fold+1))
+            path = "%s/Run%d/Fold%d" % 
