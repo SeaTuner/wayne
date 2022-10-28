a9ab735b9258dbf41ead2eff012d56c8701c6619
SeaTuner <97007035+SeaTuner@users.noreply.github.com>
2022-10-28 14:51:36 +0000

    Update P25Hosts.txt

--- rgf1.2/src/com/AzSvDataS.cpp
+++ rgf1.2/src/com/AzSvDataS.cpp
@@ -323,4 +323,171 @@ void AzSvDataS::readData_Small(const char *data_fn,
 #endif 
 
 /*------------------------------------------------------------------*/
-void A
+void AzSvDataS::readData_Large(const char *data_fn, 
+                         int expected_f_num, 
+                         /*---  output  ---*/
+                         AzSmat *m_feat,
+                         int max_data_num)
+{
+  const char *eyec = "AzSvDataS::readData_Large"; 
+
+  /*---  find the number of lines and the maximum line length  ---*/
+  AzIntArr ia_line_len; 
+  AzFile::scan(data_fn, 1024*1024, &ia_line_len, max_data_num+1);  /* +1 for sparse indicator */
+  
+  int data_num = ia_line_len.size(); 
+  int max_line_len = ia_line_len.max(); 
+  if (data_num <= 0) {
+    throw new AzException(AzInputNotValid, eyec, "Empty data"); 
+  }
+
+  AzBytArr ba_buff; 
+  AzByte *buff = ba_buff.reset(max_line_len+256, 0); /* +256 just in case */
+  
+  /*---  1st line indicates sparse/dense  ---*/
+  AzFile file(data_fn); 
+  
+  file.open("rb"); 
+  int line0_len = file.gets(buff, max_line_len); 
+  AzBytArr s_line0(buff, line0_len); 
+  
+  int line_no = 0; 
+  bool isSparse = false;
+  int f_num = if_sparse(s_line0, expected_f_num); 
+  if (f_num > 0) {
+    isSparse = true; 
+    --data_num; /* b/c 1st line is information */
+    if (data_num <= 0) {
+      throw new AzException(AzInputNotValid, eyec, "Empty sparse data file"); 
+    }
+    line_no = 1; /* first data line */
+  }
+  else {
+    f_num = expected_f_num; 
+    if (f_num <= 0) {
+      const AzByte *line0 = s_line0.point(); 
+      f_num = countFeatures(line0, line0+line0_len); 
+    }
+    if (f_num <= 0) {
+      throw new AzException(AzInputNotValid, eyec, "No feature in the first line"); 
+    }
+    file.seek(0);  /* rewind to the beginning */
+  }
+
+  /*---  read features  ---*/
+  if (max_data_num > 0) {
+    data_num = MIN(data_num, max_data_num); 
+  }
+  m_feat->reform(f_num, data_num); 
+  
+  int dx; 
+  for (dx = 0; dx < data_num; ++dx, ++line_no) {
+    int len = ia_line_len.get(line_no); 
+    file.readBytes(buff, len); 
+    buff[len] = '\0';  /* to make it a C string */
+    if (isSparse) {
+      parseDataLine_Sparse(buff, len, f_num, data_fn, line_no+1,
+                    m_feat, dx); 
+    }
+    else {
+      parseDataLine(buff, len, f_num, data_fn, line_no+1, 
+                    m_feat, dx); 
+    }
+  }
+  file.close(); 
+}                            
+
+/*------------------------------------------------------------------*/
+void AzSvDataS::_parseDataLine_Sparse(const AzByte *inp, 
+                              int inp_len, 
+                              int f_num, 
+                              const char *data_fn, 
+                              int line_no, 
+                              /*---  output  ---*/
+                              AzIFarr &ifa_ex_val) 
+{
+  const char *eyec = "AzSvDataS::_parseDataLine_Sparse"; 
+
+  AzIntArr ia_isUsed; 
+  ia_isUsed.reset(f_num, 0); 
+  int *isUsed = ia_isUsed.point_u(); 
+
+  const AzByte *wp = inp, *line_end = inp + inp_len; 
+//  AzIFarr ifa_ex_val; 
+  for ( ; ; ) {
+    if (wp >= line_end) break; 
+
+    AzBytArr str_token; 
+    AzTools::getString(&wp, line_end, &str_token); 
+    if (str_token.getLen() > 0) {
+      int ex; 
+      double val; 
+      decomposeFeat(str_token.c_str(), line_no, &ex, &val); 
+      if (ex < 0 || ex >= f_num) {
+        AzBytArr s("Error in line# "); s.cn(line_no); 
+        s.c(": invalid feature# "); s.cn(ex); 
+        throw new AzException(AzInputError, eyec, s.c_str());     
+      }
+      if (isUsed[ex]) {
+        AzBytArr s("Error in line# "); s.cn(line_no); 
+        s.c(": feature# "); s.cn(ex); s.c(" appears more than once."); 
+        throw new AzException(AzInputError, eyec, s.c_str()); 
+      }
+      if (val != 0) {
+        ifa_ex_val.put(ex, val); 
+      }
+      isUsed[ex] = 1; 
+    }
+  }
+//  m_feat->load(col, &ifa_ex_val); 
+}
+
+/*-------------------------------------------------------------*/
+/* ex:val */
+void AzSvDataS::decomposeFeat(const char *token,
+                              int line_no,  
+                              /*---  output  ---*/
+                              int *ex, 
+                              double *val)
+{
+  /* *ex = atol(token); */
+  *ex = my_fno(token, "AzSvDataS::decomposeFeat", line_no); 
+
+  *val = 1; 
+  const char *ptr = strchr(token, ':');   
+  if (ptr != NULL) {
+    /* *val = atof(ptr + 1); */
+    *val = my_atof(ptr+1, "AzSvDataS::decomposeFeat", line_no); 
+  }
+}
+
+/*------------------------------------------------------------------*/
+int AzSvDataS::if_sparse(AzBytArr &s_line, 
+                         int expected_f_num, 
+                         const char *str)
+{
+  const char *eyec = "AzSvDataS::if_sparse"; 
+
+  int sparse_f_num = -1; 
+  AzBytArr s_sparse(str);  
+  AzStrPool sp_tok; 
+  AzTools::getStrings(s_line.point(), s_line.length(), &sp_tok); 
+  if (sp_tok.size() > 0 && 
+      s_sparse.compare(sp_tok.c_str(0)) == 0) {
+    if (sp_tok.size() >= 2) {
+      sparse_f_num = atol(sp_tok.c_str(1)); 
+    }
+    if (sparse_f_num <= 0) {
+      throw new AzException(AzInputError, eyec, 
+            "1st line of sparse data file must be \"sparse dd\" where dd is the feature dimensionality."); 
+    }
+    if (expected_f_num > 0 && sparse_f_num != expected_f_num) {
+      throw new AzException(AzInputError, eyec, 
+            "Conflict in feature dim: feature definition file vs. data file.");
+    }
+  }
+  return sparse_f_num; 
+}
+
+
+/*---------------------------------
